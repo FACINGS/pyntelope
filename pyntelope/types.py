6f3c93f3c8b64e65acef52dd827ae265eda1dd0d
Faisal Bagalagel <uultradude@gmail.com>
2022-10-12 11:25:39 -0400

    feature: add wasm type
    also add necassary functions for hex/bin/unit8 array conversions

--- pyntelope/types.py
+++ pyntelope/types.py
@@ -1,11 +1,14 @@
 """Antelope data types."""
 
+import binascii
 import calendar
 import datetime as dt
 import re
 import struct
 import sys
+import zipfile
 from abc import ABC, abstractmethod
+from pathlib import Path
 
 import pydantic
 
@@ -575,6 +578,76 @@ class Varuint32(AntelopeType):
         return cls(value=value)
 
 
+class Wasm(AntelopeType):
+    value: bytes
+
+    @classmethod
+    def from_file(cls, file: Path, *, extension: str = ".wasm"):
+        """Create a wasm object from a .wasm or from a zipped file."""
+        if isinstance(file, Path):
+            fullpath = file
+        else:
+            fullpath = Path().resolve() / Path(file)
+
+        if fullpath.suffix == ".zip":
+            with zipfile.ZipFile(fullpath) as zp:
+                zip_path = str(fullpath.stem) + extension
+                with zp.open(zip_path, mode="r") as f:
+                    file_contents = f.read()
+
+        else:
+            with open(fullpath, "rb") as f:
+                file_contents = f.read()
+
+        return cls(value=file_contents)
+
+    def to_hex(self):
+        return str(binascii.hexlify(self.value).decode("utf-8"))
+
+    def __bytes__(self):
+        uint8_array = _hex_to_uint8_array(self.to_hex())
+        return bytes(uint8_array)
+
+    @classmethod
+    def from_bytes(cls, bytes_):
+        uint8_array = Array.from_bytes(bytes_=bytes_, type_=Uint8)
+        uint8_list = uint8_array.values
+        hexcode = _uint8_list_to_hex(uint8_list)
+        value = _hex_to_bin(hexcode)
+        return cls(value=value)
+
+
+def _hex_to_uint8_array(hex_string: str) -> Array:
+    if len(hex_string) % 2:
+        msg = "Odd number of hex digits in input file."
+        raise ValueError(msg)
+
+    bin_len = int(len(hex_string) / 2)
+    uint8_values = []
+
+    for i in range(0, bin_len):
+        try:
+            x = int(hex_string[(i * 2) : (i * 2 + 2)], base=16)  # NOQA: E203
+        except ValueError:
+            msg = "Issue converting hex to uint 8 array, Invalid hex string."
+            raise ValueError(msg)
+        uint8_values.append(x)
+
+    uint8_array = Array(type_=Uint8, values=uint8_values)
+    return uint8_array
+
+
+def _uint8_list_to_hex(uint8_list: list) -> str:
+    hexcode = ""
+    for int8 in uint8_list:
+        hexcode += ("00" + str(format(int8.value, "x")))[-2:]
+    return hexcode
+
+
+def _hex_to_bin(hexcode: str) -> bytes:
+    return binascii.unhexlify(hexcode.encode("utf-8"))
+
+
 def _get_all_types():
     def is_eostype(class_):
         if isinstance(class_, type):
@@ -594,6 +667,7 @@ _all_types = _get_all_types()
 
 
 def from_string(type_: str) -> AntelopeType:
+    """Return an AntelopeType object from a given string."""
     type_ = type_.lower()
     try:
         class_ = _all_types[type_]
